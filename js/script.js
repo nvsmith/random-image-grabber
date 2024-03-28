d9fb64bd4ae92f1c76d1c7995667b640655e6b9b
Nathan Smith <nathanielvs@gmail.com>
2024-03-28 10:36:54 -0600

    Set timeout for button text change. Update colors and text

--- js/script.js
+++ js/script.js
@@ -7,30 +7,33 @@ const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
 const getImage = async function () {
-  //   Enable async communication between the program and the Lorem Picsum API.
-  const res = await fetch("https://picsum.photos/v2/list?limit=100");
-  //   Parse the data captured in `res` using the json method.
-  const images = await res.json();
-  //   console.log(images);
-  selectRandomImage(images);
+    //   Enable async communication between the program and the Lorem Picsum API.
+    const res = await fetch("https://picsum.photos/v2/list?limit=100");
+    //   Parse the data captured in `res` using the json method.
+    const images = await res.json();
+    //   console.log(images);
+    selectRandomImage(images);
 };
 
 selectRandomImage = function (images) {
-  const randomIndex = Math.floor(Math.random() * images.length);
-  //   console.log(randomIndex);
-  const randomImage = images[randomIndex];
-  //   console.log(randomImage);
-  displayImage(randomImage);
+    const randomIndex = Math.floor(Math.random() * images.length);
+    //   console.log(randomIndex);
+    const randomImage = images[randomIndex];
+    //   console.log(randomImage);
+    displayImage(randomImage);
 };
 
 const displayImage = function (randomImage) {
-  const author = randomImage.author;
-  const imageAddress = randomImage.download_url;
-  authorSpan.innerText = author;
-  img.src = imageAddress;
-  imgDiv.classList.remove("hide");
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imgDiv.classList.remove("hide");
 };
 
 button.addEventListener("click", function () {
-  getImage();
+    getImage();
+    setTimeout(() => {
+        this.innerText = "Show me another One";
+    }, 2000);
 });
